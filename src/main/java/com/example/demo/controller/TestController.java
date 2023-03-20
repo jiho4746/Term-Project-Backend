d64498164a33ff68d156cb9c2afee79355cb9956
jiho-par <hkp314518@duksung.ac.kr>
2023-03-20 16:09:54 +0900

    3/20일

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,20 +1,59 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.TestRequestBodyDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("test")
 public class TestController {
+
     @GetMapping("/testGetMapping")
     public String testController(){
         return "Hello World, 박지호";
     }
+
     @GetMapping("/{id}")
     public String testControllerWithPathVariables(@PathVariable(required = false) int id)
     {
-        return "Hello World! ID " + id;
+        return "Hello World! 박지호 " + id;
+    }
+
+    @GetMapping("/testRequestParam")
+    public String testControllerRequestParam(@RequestParam(required = false)int id){
+        return "Hello World! 박지호" + id;
+    }
+
+    @GetMapping("/testRequestBody")
+    public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO){
+        return "Hello World! 박지호" + testRequestBodyDTO.getId() + "Message : " +
+                testRequestBodyDTO.getMessage();
+    }
+
+    @GetMapping("/testRequestBody")
+    public ResponseDTO<String> testControllerRequestBody(){
+        List<String> list = new ArrayList<>();
+        list.add("pjh1");
+        list.add("pjh2");
+        list.add("pjh3");
+
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        return response;
+    }
+
+    @GetMapping("/testRequestEntity")
+    public ResponseEntity<?> testControllerResponseEntity(){
+        List<String> list = new ArrayList<>();
+        list.add("pjh1");
+        list.add("pjh2");
+        list.add("pjh3");
+
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        //return ResponseEntity.ok().body(response);
+        return ResponseEntity.badRequest().body(response);
     }
 }
