43487c96409c64bc164cc32cd4bd9e39ca1df8a2
jiho-par <hkp314518@duksung.ac.kr>
2023-03-22 13:10:59 +0900

    3/22 (2.2절 완료)

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.ResponseDTO;
 import com.example.demo.dto.TodoDTO;
+import com.example.demo.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +16,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/todo")
 public class TodoController {
-    @GetMapping("/testTodo")
+    /*@GetMapping("/testTodo")
     public ResponseEntity<?>testTodo(){
         TodoDTO tododto = new TodoDTO();
         tododto.setId("123");
@@ -22,8 +24,17 @@ public class TodoController {
         tododto.setDone(true);
         List<String> list = new ArrayList<>();
 
-        //ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
-        //return ResponseEntity.ok().body(response);
         return new ResponseEntity<>(tododto, HttpStatus.OK);
+    }*/
+    @Autowired
+    private TodoService service;
+
+    @GetMapping("/test")
+    public ResponseEntity<?> testTodo(){
+        String str = service.testService();
+        List<String>list = new ArrayList<>();
+        list.add(str);
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        return ResponseEntity.ok().body(response);
     }
 }
