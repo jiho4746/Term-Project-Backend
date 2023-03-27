15cdcaf97e78f94f981ea868a40086708e0420b5
jiho4746 <jiho4746@gmail.com>
2023-03-27 15:20:54 +0900

    commit test

--- src/main/java/com/example/demo/persistence/TodoRepository.java
+++ src/main/java/com/example/demo/persistence/TodoRepository.java
@@ -2,9 +2,11 @@ package com.example.demo.persistence;
 
 import com.example.demo.model.TodoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface TodoRepository extends JpaRepository<TodoEntity, String> {
+    @Query(value = "select * from Todo t where t.id =?1")
     List<TodoEntity> findByUserId(String userId);
 }
--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -6,6 +6,10 @@ import com.example.demo.persistence.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+
+
 @Service
 public class TodoService {
     @Autowired
@@ -17,4 +21,18 @@ public class TodoService {
         TodoEntity savedEntity = repository.findById(entity.getId()).get();
         return savedEntity.getTitle();
     }
+    public List<TodoEntity> create(final TodoEntity entity){
+        if (entity ==null){
+            log.warn("Entity cannot be null.");
+            throw new RuntimeException("Entity cannot be null");
+        }
+        if (entity.getUserId() ==null){
+            log.warn("Unkown user.");
+            throw new RuntimeException("Unkown user");
+        }
+        repository.save(entity);
+        log.info("Entity Id : {} is saved", entity.getId());
+        return repository.findByUserId(entity.getUserId());
+    }
+
 }
