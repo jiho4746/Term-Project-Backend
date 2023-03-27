c1c303010539ef3fb88da87eb821880a3a5d68a3
jiho4746 <jiho4746@gmail.com>
2023-03-27 16:15:36 +0900

    3/27

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -3,13 +3,14 @@ package com.example.demo.service;
 import com.example.demo.dto.TodoDTO;
 import com.example.demo.model.TodoEntity;
 import com.example.demo.persistence.TodoRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 
-
+@Slf4j
 @Service
 public class TodoService {
     @Autowired
@@ -22,6 +23,14 @@ public class TodoService {
         return savedEntity.getTitle();
     }
     public List<TodoEntity> create(final TodoEntity entity){
+
+        validate(entity);
+        repository.save(entity);
+        log.info("Entity Id : {} is saved", entity.getId());
+        return repository.findByUserId(entity.getUserId());
+    }
+    //리팩토링한 메서드
+    private static void validate(TodoEntity entity) {
         if (entity ==null){
             log.warn("Entity cannot be null.");
             throw new RuntimeException("Entity cannot be null");
@@ -30,9 +39,8 @@ public class TodoService {
             log.warn("Unkown user.");
             throw new RuntimeException("Unkown user");
         }
-        repository.save(entity);
-        log.info("Entity Id : {} is saved", entity.getId());
-        return repository.findByUserId(entity.getUserId());
     }
-
+    public List<TodoEntity>retrieve(final String userId){
+        return repository.findByUserId(userId);
+    }
 }
